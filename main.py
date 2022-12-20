bae13cd8af2a15aea86b9be6b225598a5b48561c
Carlos Ortega González <75342750+cortega26@users.noreply.github.com>
2022-12-20 18:29:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ def scrape_polla():
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("lang=es")
-    options.add_argument("start-maximized")
     options.add_argument("--disable-extensions")
     options.add_argument("--incognito")
     options.add_argument("--disable-blink-features=AutomationControlled")
