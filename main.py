7c122e83d7eca37ad056b6c6e84711e7bab4315f
Carlos Ortega González <75342750+cortega26@users.noreply.github.com>
2022-12-23 12:07:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def scrape_polla():
         prizes = text.find_all("span", class_="prize")
         driver.close()
         prizes = [int(prize.text.strip("$").replace(".", "")) * 1000000 for prize in prizes]
-        if sum(prizes) == 0:
+        if sum(prizes) == 0: # Everytime the website is updated, prizes show 0 zero for about 2 hours
             for i in range(3):
                 time.sleep(60 * 60)
                 prizes = scrape_polla()
